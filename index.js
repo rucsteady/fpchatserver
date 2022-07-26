92dbe08d801fca474ea5eb6fb27b7e1c9af67f61
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-07-26 15:53:51 +0200

    cors *

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 const io = new Server(server, {
   cors: {
-    origin: "https://focuspositive.herokuapp.com/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
